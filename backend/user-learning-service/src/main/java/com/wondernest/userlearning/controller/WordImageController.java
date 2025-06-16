37db421dc347efdadd119a0465141078c0c867fc
Kausar Parvej <kausarparvej3@gmail.com>
2025-06-16 18:47:28 +0600

    docker update

--- backend/user-learning-service/src/main/java/com/wondernest/userlearning/controller/WordImageController.java
+++ backend/user-learning-service/src/main/java/com/wondernest/userlearning/controller/WordImageController.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost", "http://localhost:80", "http://localhost:3000"})
 public class WordImageController {
     private final WordImageService service;
 
